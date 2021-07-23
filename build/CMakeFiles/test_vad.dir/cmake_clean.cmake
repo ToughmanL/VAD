99daf7a6275c0d019341f611d51bdff8536a7567
ToughmanL <meiluosi@gmail>
2021-07-23 09:37:42 +0800

    first vad

---
+++ build/CMakeFiles/test_vad.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_vad.dir/test/fvadwav.c.o"
+  "test_vad.pdb"
+  "test_vad"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/test_vad.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
